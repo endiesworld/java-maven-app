6091ef8dc7cf564acb63f2a6b9b1f0a8c2626728
endiesworld <endiesworld@gmail.com>
2025-06-30 21:49:01 -0400

    Auto trigger

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
-// def buildJar() {
-//     echo 'building the application...'
-//     sh 'mvn package'
-// }
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
+}
 
-// def buildImage() {
-//     echo "building the docker image..."
-//     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-//         sh 'docker build -t okoro/demo-java-app:java-mav-1.3 .'
-//         sh 'echo $PASS | docker login -u $USER --password-stdin'
-//         sh 'docker push okoro/demo-java-app:java-mav-1.2'
-//     }
-// }
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t okoro/demo-java-app:java-mav-3.1 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push okoro/demo-java-app:java-mav-1.2'
+    }
+}
 
 def deployApp() {
     echo 'deploying the application...'
